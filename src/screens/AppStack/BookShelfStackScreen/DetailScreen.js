5c6fdfe295b98422d4b33e30968c8d626c9c942f
Ujjwal Kandel <ujjwal.kandel@logisparktech.com>
2022-01-31 08:30:22 +0545

    Recommendation Item Styling Changed

--- src/screens/AppStack/BookShelfStackScreen/DetailScreen.js
+++ src/screens/AppStack/BookShelfStackScreen/DetailScreen.js
@@ -5,7 +5,6 @@ import {
   KeyboardAvoidingView,
   SafeAreaView,
   StyleSheet,
-  Text,
   TouchableOpacity,
   View,
 } from 'react-native';
@@ -17,44 +16,117 @@ import Requests from '../../../components/BookShelfScreen/Requests';
 import LentToDetails from '../../../components/BookShelfScreen/LentToDetails';
 import {useAuth} from '../../../contexts/Auth';
 import {baseUrl} from '../../../services/AuthService';
-import { Spinner } from '@ui-kitten/components';
+import {Spinner, Text} from '@ui-kitten/components';
 
 const DetailScreen = () => {
   const route = useRoute();
   const auth = useAuth();
   const [loading, setLoading] = useState(false);
-  const [recommendations, setRecommendations] = useState([]);
+  const [recommendations, setRecommendations] = useState([
+    {
+      id: 2,
+      isbn: '439554934',
+      isbn13: '9780439554930.0',
+      authors: 'J.K. Rowling, Mary GrandPré',
+      original_title: "Harry Potter and the Philosopher's Stone",
+      title: "Harry Potter and the Sorcerer's Stone (Harry Potter, #1)",
+      year_of_publication: '1997.0',
+      image_url: 'https://images.gr-assets.com/books/1474154022m/3.jpg',
+      createdAt: '2021-12-18T15:51:16.152Z',
+      updatedAt: '2021-12-18T15:51:16.152Z',
+    },
+    {
+      id: 5,
+      isbn: '743273567',
+      isbn13: '9780743273560.0',
+      authors: 'F. Scott Fitzgerald',
+      original_title: 'The Great Gatsby',
+      title: 'The Great Gatsby',
+      year_of_publication: '1925.0',
+      image_url: 'https://images.gr-assets.com/books/1490528560m/4671.jpg',
+      createdAt: '2021-12-18T15:51:16.152Z',
+      updatedAt: '2021-12-18T15:51:16.152Z',
+    },
+    {
+      id: 8,
+      isbn: '316769177',
+      isbn13: '9780316769170.0',
+      authors: 'J.D. Salinger',
+      original_title: 'The Catcher in the Rye',
+      title: 'The Catcher in the Rye',
+      year_of_publication: '1951.0',
+      image_url: 'https://images.gr-assets.com/books/1398034300m/5107.jpg',
+      createdAt: '2021-12-18T15:51:16.152Z',
+      updatedAt: '2021-12-18T15:51:16.152Z',
+    },
+    {
+      id: 10,
+      isbn: '679783261',
+      isbn13: '9780679783270.0',
+      authors: 'Jane Austen',
+      original_title: 'Pride and Prejudice',
+      title: 'Pride and Prejudice',
+      year_of_publication: '1813.0',
+      image_url: 'https://images.gr-assets.com/books/1320399351m/1885.jpg',
+      createdAt: '2021-12-18T15:51:16.152Z',
+      updatedAt: '2021-12-18T15:51:16.152Z',
+    },
+    {
+      id: 14,
+      isbn: '452284244',
+      isbn13: '9780452284240.0',
+      authors: 'George Orwell',
+      original_title: 'Animal Farm: A Fairy Story',
+      title: 'Animal Farm',
+      year_of_publication: '1945.0',
+      image_url: 'https://images.gr-assets.com/books/1424037542m/7613.jpg',
+      createdAt: '2021-12-18T15:51:16.152Z',
+      updatedAt: '2021-12-18T15:51:16.152Z',
+    },
+    {
+      id: 103,
+      isbn: '',
+      isbn13: '',
+      authors: 'Tina Fey',
+      original_title: 'Bossypants',
+      title: 'Bossypants',
+      year_of_publication: '2011.0',
+      image_url: 'https://images.gr-assets.com/books/1481509554m/9418327.jpg',
+      createdAt: '2021-12-18T15:51:16.152Z',
+      updatedAt: '2021-12-18T15:51:16.152Z',
+    },
+  ]);
 
-  useEffect(() => {
-    console.log(route.params.requests, 'requests');
-    getRecommendations();
-  }, []);
+  // useEffect(() => {
+  //   console.log(route.params.requests, 'requests');
+  //   getRecommendations();
+  // }, []);
 
   const getRecommendations = () => {
-    if(!loading){
+    if (!loading) {
       setLoading(true);
       axios
         .get(`${baseUrl}/book-shelf/recommendations`, {
           headers: {
-            Authorization: `Bearer ${auth.authData.token}`
-          }
+            Authorization: `Bearer ${auth.authData.token}`,
+          },
         })
         .then(response => {
-          if(response.data.message.length > 0) {
-            console.log('recommendationsssssssss', response.data.message)
+          if (response.data.message.length > 0) {
+            console.log('recommendationsssssssss', response.data.message);
             setRecommendations(response.data.message);
           }
         })
-        .catch(error=>{
-          console.log(error)
+        .catch(error => {
+          console.log(error);
         })
-        .finally(()=>{
-          setTimeout(()=>{
-            setLoading(false)
+        .finally(() => {
+          setTimeout(() => {
+            setLoading(false);
           }, 1000);
-        })
+        });
     }
-  }
+  };
 
   if (!route.params.bookData) {
     return null;
@@ -66,43 +138,47 @@ const DetailScreen = () => {
         <BookImage book={route.params.bookData} />
         <Requests requests={route.params.requests} />
         <LentToDetails lent={route.params.lent} />
+        <BookRecommendation
+          loading={loading}
+          recommendations={recommendations}
+        />
       </KeyboardAvoidingView>
-      <BookRecommendation loading={loading} recommendations={recommendations}/>
     </SafeAreaView>
   );
 };
 
-const BookRecommendation = ({loading, recommendations}) =>{
+const BookRecommendation = ({loading, recommendations}) => {
   const renderItem = ({item}) => {
+    return <BookRecommendationCard book={item} />;
+  };
+
+  const BookRecommendationCard = ({book}) => {
     return (
-      <BookRecommendationCard
-        book={item}
-      />
+      <View
+        style={{
+          alignItems: 'center',
+          height: '50%',
+          aspectRatio: 0.75,
+        }}>
+        <TouchableOpacity
+          style={{alignItems: 'center', backgroundColor: '#0f0'}}>
+          <Image
+            source={{uri: book.image_url}}
+            style={{
+              width: 100,
+              height: '100%',
+              resizeMode: 'contain',
+            }}
+          />
+          <Text category="s1" numberOfLines={2}>
+            {book.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
     );
   };
 
-  const BookRecommendationCard = ({book}) =>{
-    return (
-      <TouchableOpacity>
-        <View 
-          style={{
-            height: 150,
-            marginHorizontal: 20,
-            borderRadius: 15
-        }}>
-          <View 
-            style={{
-              alignItems: 'center'
-            }}
-          >
-            <Image source={{uri: book.image_url}} style={{width: 90, height: '100%', resizeMode: 'contain'}}></Image>
-          </View>
-        </View>
-      </TouchableOpacity>
-    )
-  }
-
-  if(loading){
+  if (loading) {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Spinner size="giant" />
@@ -110,20 +186,20 @@ const BookRecommendation = ({loading, recommendations}) =>{
     );
   }
   return (
-    <View style={{width: '100%', paddingVertical: '5%', backgroundColor: '#fff'}}>
-      <View style={{paddingLeft: '5%', paddingBottom: '3%'}}><Text style={{fontWeight: 'bold'}}>Book Recommendations</Text></View>
+    <View style={{width: '100%', padding: '5%', backgroundColor: '#fff'}}>
+      <View>
+        <Text style={{fontWeight: 'bold'}}>Book Recommendations</Text>
+      </View>
       <FlatList
         horizontal={true}
         data={recommendations}
         renderItem={renderItem}
-        ItemSeparatorComponent={({highlighted}) => (
-          <View style={{height: 10, width: 10}} />
-        )}
+        ItemSeparatorComponent={({highlighted}) => <View style={{width: 10}} />}
         keyExtractor={(item, index) => index.toString()}
       />
     </View>
-  )
-}
+  );
+};
 
 export default DetailScreen;
 
